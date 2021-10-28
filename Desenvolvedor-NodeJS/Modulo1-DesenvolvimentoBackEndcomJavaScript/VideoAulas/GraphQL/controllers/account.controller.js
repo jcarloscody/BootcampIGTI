aa3563b22d58ed01cd432e351c9ad7939dedb0e1
Josue <josuecarlosos2@gmail.com>
2021-10-28 15:02:10 -0300

    graphql

---
+++ Desenvolvedor-NodeJS/Modulo1-DesenvolvimentoBackEndcomJavaScript/VideoAulas/GraphQL/controllers/account.controller.js
@@ -0,0 +1,81 @@
+import AccountServices from '../services/account.service.js'
+
+
+async function createAccount(req, res, next) {
+    try {
+        let account = req.body
+        if ((!account.nome || !account.balance == null)) {
+            throw new Error("o nome e o balance são itens obrigatorios")
+        }
+
+        account = await AccountServices.createAccount(account)
+        res.send(account)
+
+        logger.info(`POST /account ${JSON.stringify(account)}`)
+    } catch (error) {
+        next(error)
+    }
+}
+
+async function getAccounts(req, res, next) {
+    try {
+        res.send(await AccountServices.getAccounts())
+        logger.info(`GET /account `)
+    } catch (error) {
+        next(error)
+    }
+}
+
+async function getAccount(req, res, next) {
+    try {
+        res.send(await AccountServices.getAccount(req.params.id));
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+async function deleteAccount(req, res, next) {
+    try {
+        res.send(await AccountServices.deleteAccount(req.params.id));
+    } catch (error) {
+        next(error)
+    }
+}
+
+async function updateAccount(req, res, next) {
+    try {
+        let accountBody = req.body;
+        if ((!accountBody.id || !accountBody.nome || !accountBody.balance == null)) {
+            throw new Error("o ID, nome e o balance são itens obrigatorios")
+        }
+        res.send(await AccountServices.updateAccount(accountBody.id, accountBody.nome, accountBody.balance))
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+async function updateBalance(req, res, next) {
+    try {
+        let accountBody = req.body;
+        if ((!accountBody.id || !accountBody.balance == null)) {
+            throw new Error("o ID e o balance são itens obrigatorios")
+        }
+        res.send(await AccountServices.updateBalance(accountBody.id, accountBody.balance))
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+
+
+export default {
+    createAccount,
+    getAccounts,
+    getAccount,
+    deleteAccount,
+    updateAccount,
+    updateBalance
+};
---
+++ Desenvolvedor-NodeJS/Modulo1-DesenvolvimentoBackEndcomJavaScript/VideoAulas/GraphQL/index.js
@@ -0,0 +1,99 @@
+//vamos usar o GraphQL -- npm i graphql --save     --  tbm usaremos o express-graphql   npm i express-graphql --save
+import express from 'express';
+import winston from 'winston';
+import { routerAccount } from './routes/account.routes.js';
+import { promises as fs } from 'fs';
+import cors from 'cors'; //responsável por permitir que outros dominios consuma seus recursos
+import swagger from 'swagger-ui-express'
+import { swaggerDocument } from './doc.js';
+import { buildSchema } from 'graphql';
+import { graphqlHTTP } from 'express-graphql';
+
+
+const { readFile, writeFile } = fs
+
+global.fileName = "accounts.json";
+
+//Fazendo o logger
+const { label, timestamp, combine, printf } = winston.format;
+
+const myFormat = printf(({ level, message, label, timestamp }) => {
+    return `${timestamp} --${label}-- ${level}: ${message}`
+})
+
+global.logger = winston.createLogger({
+    level: "silly",
+    transports: [
+        new (winston.transports.Console)(),
+        new (winston.transports.File)({ filename: "my-bank-api.log" })
+    ],
+    format: combine(
+        label({ label: "my-bank-api" }),
+        timestamp(),
+        myFormat
+    )
+})
+
+const schemaGraph = buildSchema(`
+    type Account {
+        id: Int
+        nome: String
+        balance: Float
+    }
+    type Query {
+        getAccounts: [Account]
+        getAccount(id: int): Account
+    }
+`)
+
+//instanciando o server 
+const app = express();
+app.use(express.json())
+app.use(express.static("public"))
+app.use(cors()); //libera todos os endpoints
+app.use("/doc", swagger.serve, swagger.setup(swaggerDocument))
+
+app.use("/account", routerAccount);
+
+app.use("/graphql", graphqlHTTP({
+    schema: schemaGraph,
+    rootValue: null,
+    graphiql: true //tras uma interface
+}))
+
+
+app.listen(3001, async () => {
+
+
+    try {
+        await readFile(global.fileName);
+        logger.info("API Started!")
+    } catch (error) {
+        const initialJson = {
+            nextId: 1,
+            accounts: []
+        }
+
+        writeFile(global.fileName, JSON.stringify(initialJson)).then(() => {
+            logger.info("API Started and File created!")
+
+        }).catch((erro) => {
+            logger.error(erro)
+        });
+    }
+
+    console.log("API Started!")
+})
+
+/**
+ * cors - compartilhamento de recursos de origem cruzadas. é uma questao de seguranca que temos em paginas web
+ * que quando vc fornece uma api por default somente paginas que estejam hospedadas juntamente a esta api que podem
+ * conseguir acessar ela.. EM SUMA uma api de outro dominio esta quer consumir os recurso da nossa api que esta em dominio diferente
+ * por padrão será bloqueado, para liberar usamos a biblioteca cors
+ */
+
+/**DOCUMENTAÇÃO
+ * editor.swagger.io
+ *
+ * bibliboteca: npm install swagger-ui-express
+ */
