ca76315eed2afda8d478f624c10086de7f95c287
Josue <josuecarlosos2@gmail.com>
2021-11-06 20:07:19 -0300

    revisando

---
+++ Desenvolvedor-NodeJS/Modulo1-DesenvolvimentoBackEndcomJavaScript/VideoAulas/GraphQL/test.js
@@ -0,0 +1,19 @@
+var x = "lskd"
+function name() {
+
+    console.log(x)
+}
+
+function name2() {
+    var xx = "lksd"
+    function n3(params) {
+        var xx = "aqui"
+        console.log(xx)
+        var r = "sdsd"
+    }
+    n3()
+
+}
+
+name()
+name2()
